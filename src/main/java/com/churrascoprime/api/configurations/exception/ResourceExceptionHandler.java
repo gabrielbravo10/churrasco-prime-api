559ec27fb6960fd346cafbb67a56d53fba474d8f
Tatu-Armadillo <prohazor.fa@gmail.com>
2021-05-29 11:39:00 -0300

    Limpeza no codigo

--- src/main/java/com/churrascoprime/api/configurations/exception/ResourceExceptionHandler.java
+++ src/main/java/com/churrascoprime/api/configurations/exception/ResourceExceptionHandler.java
@@ -4,20 +4,16 @@ package com.churrascoprime.api.configurations.exception;
 import com.churrascoprime.api.dtos.error.ErrorDto;
 import com.churrascoprime.api.exceptions.NotAnImageFileException;
 import com.churrascoprime.api.exceptions.RecordNotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
