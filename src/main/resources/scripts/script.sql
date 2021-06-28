e7310c28b34cc4488d4d055e0127043f3f4f709e
Gabriel Bravo <>
2021-06-28 19:39:09 -0400

    sql script updated

--- src/main/resources/scripts/script.sql
+++ src/main/resources/scripts/script.sql
@@ -1,169 +1,164 @@
-drop database if exists churrasco_prime;
-create database churrasco_prime;
-use churrasco_prime;
-
-DROP TABLE IF EXISTS tb_provider;
-CREATE TABLE tb_provider(
-    id_provider int AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    cnpj varchar(255) DEFAULT NULL,
-    image_url varchar(255) DEFAULT NULL,
-    active tinyint(1) DEFAULT 0,
-    rating decimal(6,2) DEFAULT NULL,
-    date_created timestamp NOT NULL,
-    date_updated timestamp,
-    date_deleted timestamp,
-    PRIMARY KEY (id_provider),
-    UNIQUE KEY (name),
-    UNIQUE KEY (cnpj)
+create table tb_category
+(
+    id_category int auto_increment
+        primary key,
+    name varchar(255) not null,
+    date_created timestamp not null,
+    date_updated timestamp null,
+    date_deleted timestamp null,
+    constraint name
+        unique (name)
 );
 
-DROP TABLE IF EXISTS tb_category;
-create table tb_category(
-    id_category int NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    date_created timestamp NOT NULL,
-    date_updated timestamp,
-    date_deleted timestamp,
-    PRIMARY KEY (id_category),
-    UNIQUE KEY (name)
+create table tb_city
+(
+    id_city int auto_increment
+        primary key,
+    name varchar(255) not null,
+    date_created timestamp not null,
+    date_updated timestamp null,
+    date_deleted timestamp null,
+    constraint name
+        unique (name)
 );
 
-DROP TABLE IF EXISTS tb_product;
-CREATE TABLE tb_product(
-    id_product int AUTO_INCREMENT,
-    sku varchar(255) NOT NULL,
-    name varchar(255) DEFAULT NULL,
-    description varchar(255) DEFAULT NULL,
-    unit_price decimal(6,2) DEFAULT NULL,
-    image_url varchar(255) DEFAULT NULL,
-    active tinyint(1) DEFAULT 0,
-    units_in_stock int DEFAULT NULL,
-    id_provider int NOT NULL,
-    date_created timestamp NOT NULL,
-    date_updated timestamp,
-    date_deleted timestamp,
-    PRIMARY KEY (id_product),
-    CONSTRAINT fk_product_provider FOREIGN KEY (id_provider) REFERENCES tb_provider(id_provider)
+create table tb_customer
+(
+    id_customer int auto_increment
+        primary key,
+    cpf varchar(255) null,
+    first_name varchar(255) not null,
+    last_name varchar(255) null,
+    email varchar(255) null,
+    password varchar(255) null,
+    date_created timestamp not null,
+    date_updated timestamp null,
+    date_deleted timestamp null,
+    telefone varchar(11) null,
+    constraint cpf
+        unique (cpf),
+    constraint email
+        unique (email)
 );
 
-DROP TABLE IF EXISTS tb_city;
-CREATE TABLE tb_city (
-    id_city int AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    date_created timestamp NOT NULL,
-    date_updated timestamp,
-    date_deleted timestamp,
-    PRIMARY KEY (id_city),
-    UNIQUE KEY (name)
+create table tb_address
+(
+    id_address int auto_increment
+        primary key,
+    zip_code varchar(255) null,
+    street varchar(255) null,
+    number varchar(255) null,
+    complement varchar(255) null,
+    neighborhood varchar(255) null,
+    id_city int not null,
+    id_customer int not null,
+    date_created timestamp not null,
+    date_updated timestamp null,
+    date_deleted timestamp null,
+    constraint fk_address_city
+        foreign key (id_city) references tb_city (id_city),
+    constraint fk_address_customer
+        foreign key (id_customer) references tb_customer (id_customer)
 );
 
-DROP TABLE IF EXISTS tb_customer;
-CREATE TABLE tb_customer(
-    id_customer int AUTO_INCREMENT,
-    cpf varchar(255) DEFAULT NULL,
-    first_name varchar(255) NOT NULL,
-    last_name varchar(255) DEFAULT NULL,
-    email varchar(255) DEFAULT NULL,
-    password varchar(255) DEFAULT NULL,
-    date_created timestamp NOT NULL,
-    date_updated timestamp,
-    date_deleted timestamp,
-    PRIMARY KEY (id_customer),
-    UNIQUE KEY (email),
-    UNIQUE KEY (cpf)
+create index id_city
+	on tb_address (id_city);
+
+create index id_customer
+	on tb_address (id_customer);
+
+create table tb_order
+(
+    id_order int auto_increment
+        primary key,
+    order_tracking_number varchar(255) null,
+    total_price decimal(19,2) null,
+    total_quantity int null,
+    order_status varchar(128) null,
+    order_payment varchar(128) null,
+    id_customer int null,
+    id_address int null,
+    date_created timestamp not null,
+    date_updated timestamp null,
+    date_deleted timestamp null,
+    constraint tb_order_ibfk_1
+        foreign key (id_customer) references tb_customer (id_customer),
+    constraint tb_order_ibfk_2
+        foreign key (id_address) references tb_address (id_address)
 );
 
-DROP TABLE IF EXISTS tb_address;
-CREATE TABLE tb_address(
-    id_address int AUTO_INCREMENT,
-    zip_code varchar(255) DEFAULT NULL,
-    street varchar(255) DEFAULT NULL,
-    number varchar(255) DEFAULT NULL,
-    complement varchar(255) DEFAULT NULL,
-    neighborhood varchar(255) DEFAULT NULL,
-    id_city int NOT NULL,
-    id_customer int NOT NULL,
-    date_created timestamp NOT NULL,
-    date_updated timestamp,
-    date_deleted timestamp,
-    PRIMARY KEY (id_address),
-    KEY (id_city),
-    KEY (id_customer),
-    CONSTRAINT fk_address_city FOREIGN KEY (id_city) REFERENCES tb_city(id_city),
-    CONSTRAINT fk_address_customer FOREIGN KEY (id_customer) REFERENCES tb_customer(id_customer)
+create index id_address
+	on tb_order (id_address);
+
+create index id_customer
+	on tb_order (id_customer);
+
+create table tb_provider
+(
+    id_provider int auto_increment
+        primary key,
+    name varchar(255) not null,
+    cnpj varchar(255) null,
+    image_url varchar(255) null,
+    active tinyint(1) default 0 null,
+    rating decimal(6,2) null,
+    date_created timestamp not null,
+    date_updated timestamp null,
+    date_deleted timestamp null,
+    constraint cnpj
+        unique (cnpj),
+    constraint name
+        unique (name)
 );
 
-DROP TABLE IF EXISTS tb_telephone;
-CREATE TABLE tb_telephone(
-    id_telephone int AUTO_INCREMENT,
-    telephone_number varchar(255) NOT NULL,
-    id_customer int NOT NULL,
-    date_created timestamp NOT NULL,
-    date_updated timestamp,
-    date_deleted timestamp,
-    PRIMARY KEY (id_telephone),
-    KEY (id_customer),
-    FOREIGN KEY (id_customer) REFERENCES tb_customer(id_customer),
-    CONSTRAINT fk_telephone_customer FOREIGN KEY (id_customer) REFERENCES tb_customer(id_customer)
+create table tb_product
+(
+    id_product int auto_increment
+        primary key,
+    sku varchar(255) not null,
+    name varchar(255) null,
+    description varchar(255) null,
+    unit_price decimal(6,2) null,
+    image_url varchar(255) null,
+    active tinyint(1) default 0 null,
+    units_in_stock int null,
+    id_provider int not null,
+    date_created timestamp not null,
+    date_updated timestamp null,
+    date_deleted timestamp null,
+    constraint fk_product_provider
+        foreign key (id_provider) references tb_provider (id_provider)
 );
 
-DROP TABLE IF EXISTS tb_product_category;
-CREATE TABLE tb_product_category(
-    id_product int NOT NULL,
-    id_category int NOT NULL,
-    PRIMARY KEY (id_product, id_category),
-    CONSTRAINT fk_product_category_product FOREIGN KEY (id_product) REFERENCES tb_product (id_product),
-    CONSTRAINT fk_product_category_category FOREIGN KEY (id_category) REFERENCES tb_category (id_category)
+create table tb_order_items
+(
+    id_order_items int auto_increment
+        primary key,
+    image_url varchar(255) null,
+    discount decimal(6,2) null,
+    price decimal(6,2) null,
+    quantity int null,
+    id_order int not null,
+    id_product int not null,
+    constraint tb_order_items_ibfk_1
+        foreign key (id_order) references tb_order (id_order),
+    constraint tb_order_items_ibfk_2
+        foreign key (id_product) references tb_product (id_product)
 );
 
-DROP TABLE IF EXISTS tb_order;
-CREATE TABLE tb_order(
-    id_order int  AUTO_INCREMENT,
-    order_tracking_number varchar(255) DEFAULT NULL,
-    total_price decimal(19,2) DEFAULT NULL,
-    total_quantity int DEFAULT NULL,
-    order_status varchar(128) DEFAULT NULL,
-    order_payment varchar(128) DEFAULT NULL,
-    rating decimal(6,2) DEFAULT NULL,
-    id_customer int DEFAULT NULL,
-    id_address int DEFAULT NULL,
-    date_created timestamp NOT NULL,
-    date_updated timestamp,
-    date_deleted timestamp,
-    PRIMARY KEY (id_order),
-    KEY (id_customer),
-    KEY (id_address),
-    FOREIGN KEY (id_customer) REFERENCES tb_customer(id_customer),
-    FOREIGN KEY (id_address) REFERENCES tb_address(id_address)
-);
+create index id_order
+	on tb_order_items (id_order);
 
-DROP TABLE IF EXISTS tb_order_items;
-CREATE TABLE tb_order_items(
-    id_order_items int auto_increment,
-    image_url varchar(255) DEFAULT NULL,
-    discount decimal(6,2) DEFAULT NULL,
-    price decimal(6,2) DEFAULT NULL,
-    quantity int DEFAULT NULL,
-    id_order int NOT NULL,
-    id_product int NOT NULL,
-    PRIMARY KEY (id_order_items),
-    KEY (id_product),
-    FOREIGN KEY (id_order) REFERENCES tb_order (id_order),
-    FOREIGN KEY (id_product) REFERENCES tb_product (id_product)
-);
+create index id_product
+	on tb_order_items (id_product);
 
-DROP TABLE IF EXISTS tb_delivery;
-CREATE TABLE tb_delivery(
-    id int AUTO_INCREMENT,
-    delivery_number varchar(255) DEFAULT NULL,
-    departure datetime(6) DEFAULT NULL,
-    arrival datetime(6) DEFAULT NULL,
-    rating decimal(6,2) DEFAULT NULL,
-    id_order int NOT NULL,
-    date_created timestamp NOT NULL,
-    date_updated timestamp,
-    date_deleted timestamp,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_order) REFERENCES tb_order (id_order)
+create table tb_product_category
+(
+    id_product int not null,
+    id_category int not null,
+    primary key (id_product, id_category),
+    constraint fk_product_category_category
+        foreign key (id_category) references tb_category (id_category),
+    constraint fk_product_category_product
+        foreign key (id_product) references tb_product (id_product)
 );
